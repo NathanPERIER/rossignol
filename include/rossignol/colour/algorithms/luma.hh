279297f36aa2a3f558c320f170b30fc82ea2926a
NathanPERIER <periernathan2000@gmail.com>
2026-05-05 20:02:52 +0200

    Update the luma method to use a more generic channel combination method

--- include/rossignol/colour/algorithms/luma.hh
+++ include/rossignol/colour/algorithms/luma.hh
@@ -1,24 +1,7 @@
 
 #pragma once
 
-#include "rossignol/colour/rgb.hh"
-
-
-namespace rol::detail {
-
-constexpr uint32_t luma_coeff_factor = 10000;
-
-template <uint32_t RedCoeff, uint32_t GreenCoeff, uint32_t BlueCoeff>
-requires((0 <= RedCoeff && RedCoeff <= luma_coeff_factor) && (0 <= GreenCoeff && GreenCoeff <= luma_coeff_factor) && (0 <= BlueCoeff && BlueCoeff <= luma_coeff_factor) && (RedCoeff + BlueCoeff + GreenCoeff == luma_coeff_factor))
-uint8_t luma_impl(const rgba& col) {
-    return static_cast<uint8_t>((
-          static_cast<uint32_t>(col.r) * RedCoeff
-        + static_cast<uint32_t>(col.g) * GreenCoeff
-        + static_cast<uint32_t>(col.b) * BlueCoeff
-    ) / luma_coeff_factor);
-}
-
-} // namespace rol::detail
+#include "rossignol/colour/algorithms/combine_channels.hh"
 
 
 namespace rol {
@@ -32,12 +15,12 @@ enum class luma_method {
 template <luma_method Luma>
 uint8_t luma(const rgba& col) {
     if constexpr (Luma == luma_method::sdtv) {
-        return detail::luma_impl<2990, 5870, 1140>(col);
+        return detail::combine_channels<10000, 2990, 5870, 1140>(col);
     } else if constexpr (Luma == luma_method::hdtv) {
-        return detail::luma_impl<2126, 7152,  722>(col);
+        return detail::combine_channels<10000, 2126, 7152,  722>(col);
     } else {
         static_assert(Luma == luma_method::hdr);
-        return detail::luma_impl<2627, 6780,  593>(col);
+        return detail::combine_channels<10000, 2627, 6780,  593>(col);
     }
 }
 
