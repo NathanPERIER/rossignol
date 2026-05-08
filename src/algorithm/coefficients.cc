953d058e2b4f435b8683ceac2fed6e4e82aae143
NathanPERIER <periernathan2000@gmail.com>
2026-05-09 01:34:35 +0200

    TO SQUASH -2

--- src/algorithm/coefficients.cc
+++ src/algorithm/coefficients.cc
@@ -3,10 +3,10 @@
 #include "image/algorithm/pixel_mapper.hh"
 
 
-namespace rol::detail {
+namespace rol::algo {
 
 coefficient_plane to_coefficients(const layer& img) {
     return rol::detail::map_pixels<double>(img, [](const uint8_t& pixel) { return static_cast<double>(pixel) / 255.0; });
 }
 
-} // namespace rol::detail
+} // namespace rol::algo
