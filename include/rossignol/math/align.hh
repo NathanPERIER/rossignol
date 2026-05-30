95f1ac464b1f8f8681df57303f79cf56eb9de4e9
NathanPERIER <periernathan2000@gmail.com>
2026-05-31 00:44:16 +0200

    Simplified alignment computation to enable dissociating the vertical and horizontal components

--- include/rossignol/math/align.hh
+++ include/rossignol/math/align.hh
@@ -7,33 +7,28 @@
 
 namespace rol::math {
 
-template <horizontal_alignment AlignX, vertical_alignment AlignY>
-rect align(const vec2u& size, const rect& reference) {
-    vec2i p1;
-	vec2i p2;
-	if constexpr (AlignX == horizontal_alignment::left) {
-		p1.x = reference.x1();
-		p2.x = p1.x + static_cast<int64_t>(size.x);
-	} else if constexpr (AlignX == horizontal_alignment::right) {
-		p2.x = reference.x2();
-		p1.x = p2.x - static_cast<int64_t>(size.x);
-	} else {
-		static_assert(AlignX == horizontal_alignment::centre);
-		p1.x = (static_cast<int64_t>(size.x) - static_cast<int64_t>(reference.width())) / 2;
-		p2.x = p1.x + static_cast<int64_t>(size.x);
-	}
-	if constexpr (AlignY == vertical_alignment::top) {
-		p1.y = reference.y1();
-		p2.y = p1.y + static_cast<int64_t>(size.y);
-	} else if constexpr (AlignY == vertical_alignment::bottom) {
-		p2.y = reference.y2();
-		p1.y = p2.y - static_cast<int64_t>(size.y);
-	} else {
-		static_assert(AlignY == vertical_alignment::centre);
-		p1.y = (static_cast<int64_t>(size.y) - static_cast<int64_t>(reference.height())) / 2;
-		p2.y = p1.y + static_cast<int64_t>(size.y);
-	}
-    return rect(p1, p2);
+template <horizontal_alignment AlignX>
+int64_t aligned_horizontal_offset(std::size_t size, std::size_t reference_size) {
+    if constexpr (AlignX == horizontal_alignment::left) {
+        return 0;
+    } else if constexpr (AlignX == horizontal_alignment::right) {
+        return static_cast<int64_t>(reference_size) - static_cast<int64_t>(size);
+    } else {
+        static_assert(AlignX == horizontal_alignment::centre);
+        return (static_cast<int64_t>(reference_size) - static_cast<int64_t>(size)) / 2;
+    }
+}
+
+template <vertical_alignment AlignY>
+int64_t aligned_vertical_offset(std::size_t size, std::size_t reference_size) {
+    if constexpr (AlignY == vertical_alignment::top) {
+        return 0;
+    } else if constexpr (AlignY == vertical_alignment::bottom) {
+        return static_cast<int64_t>(reference_size) - static_cast<int64_t>(size);
+    } else {
+        static_assert(AlignY == vertical_alignment::centre);
+        return (static_cast<int64_t>(reference_size) - static_cast<int64_t>(size)) / 2;
+    }
 }
 
 } // namespace rol::math
